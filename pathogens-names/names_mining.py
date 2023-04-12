d25800819814d315d0de6a2d987d9b716cfcb685
Artur Pedro <arturpedromartins@gmail.com>
2023-04-12 11:36:51 -0300

    Comentários adicionados

--- pathogens-names/names_mining.py
+++ pathogens-names/names_mining.py
@@ -2,6 +2,8 @@ from bs4 import BeautifulSoup
 import os
 import sys
 
+# Possíveis inputs: ncbi_pat; add; remove; list
+
 operation = sys.argv[1]
 if operation == 'add' or operation == 'remove':
     species = sys.argv[2]
@@ -17,8 +19,8 @@ if (operation == "ncbi_pat") or not operation:
     # Abrindo o html e gerando um .html somente com as tags <a>
     with open("pathogens-names/index.html","r") as html:
         soup = BeautifulSoup(html, 'html.parser')
-        
         tags = soup.find_all('a')
+        # Escrevendo um arquivo somente com as tags filtradas
         with open("pathogens-names/filtered.html", "w") as t:
             for a in tags:
                 t.write(str(a))
@@ -26,15 +28,21 @@ if (operation == "ncbi_pat") or not operation:
 
     # Lista de nomes iniciada
     names =[]
+    
     # Filtrando somente os nomes das espécies do .html e alimentando a lista
     with open("pathogens-names/filtered.html") as html:
         soup = BeautifulSoup(html,'html.parser')
+        
+        # Encontrando somente os <a> que representam repositórios
         for tag in soup.find_all("a"):
             content = tag.get_text()
             if content[-1] == "/":
                 names.append(content[0:-1])
+        
+        # Atualizar lista
         if os.path.exists("pathogens-names/species_names.txt"):
             with open("pathogens-names/species_names.txt","r") as rfile:
+                # Nomes já no arquivo
                 names_infile = [line.rstrip("\n") for line in rfile.readlines()]
                 with open("pathogens-names/species_names.txt","w") as file:
                     new_names = names_infile
@@ -45,24 +53,26 @@ if (operation == "ncbi_pat") or not operation:
                     for name in new_names:
                         file.write(name + "\n")
         else:
-            readmode = "w"
-            with open("pathogens-names/species_names.txt",readmode) as file:
+            with open("pathogens-names/species_names.txt","w") as file:
                 for name in names:
                         file.write(name+"\n")
     os.system('rm pathogens-names/filtered.html')
 
+# Adicionar nome manualmente a lista
 if operation == "add" and species:
     with open("pathogens-names/species_names.txt", "r") as names:
         all = names.readlines()
+        # Checando se o nome já consta na lista
         if f"{species}\n" in all:
             print(f"{species} já está na lista")
         else:    
             all.append(species+"\n")
-            with open("pathogens-names/species_names.txt","w") as new_names:
+            with open("pathogens-names/species_names.txt","w") as new_file:
                 for name in sorted(all):
-                    new_names.write(name)
+                    new_file.write(name)
                 print(species+" adicionada ao species_name.txt")
 
+# Remover nome manualmente da lista
 if operation == "remove" and species:
     with open("pathogens-names/species_names.txt", "r") as names:
         all = [line.rstrip("\n") for line in names.readlines()]
@@ -74,6 +84,3 @@ if operation == "remove" and species:
                 print(species+" removida do species_name.txt")
         else:
             print(species+" não está na lista")
-    
-        
-         
