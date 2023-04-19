ccc5730eb91bd7a3f62a1dce12f6fecb03bd7cf2
Artur Pedro <arturpedromartins@gmail.com>
2023-04-19 11:37:59 -0300

    ids-mining.py em fase de testes

--- pathogens-ids/ids-mining.py
+++ pathogens-ids/ids-mining.py
@@ -18,22 +18,18 @@ class Species():
 
     def log_ids(self):
         #aqui eu vou pegar todos os IDs e atualizar o log com possíveis novos 
-        if not os.path.exists(f'logs/{self.name}_log_ids.txt'):
-            with open(f'logs/{self.name}_log_ids.txt','w') as log:
-                log.write(f'{self.name} | número de assemblies: {self.count} | date: {datetime.now()}')
-                for i,j in self.assembly_ids,self.biosample_ids:
-                    log.write(f'{i},{j}')
-            print(f'\n\nLOG de {self.name} gerado\n\n')
-        else:
-            self.refresh_ids()
-
+        with open(f'pathogens-ids/logs/{self.name}_log_ids.txt','w') as log:
+            log.write(f'{self.name} | número de assemblies: {self.count} | date: {datetime.now()}\n')
+            for i in self.ids:
+                log.write(f'{i[0]} {i[1]}\n')
+        print(f'\n\nLOG de {self.name} gerado\n\n')
     
     def extract_ids(self):
         # extraindo os ids e colocando em um arquivo temporário
-        os.system(f"esearch -db assembly -query '\"{self.name}\"[Organism] AND (latest[filter] AND \"complete genome\"[filter] AND all[filter] NOT anomalous[filter])' | efetch -format docsum |xtract -pattern DocumentSummary -element AssemblyAccession -element BioSampleAccn > {self.name}_temp_ids.txt")
+        os.system(f"esearch -db assembly -query '\"{self.name}\"[Organism] AND (latest[filter] AND \"complete genome\"[filter] AND all[filter] NOT anomalous[filter])' | efetch -format docsum |xtract -pattern DocumentSummary -element AssemblyAccession -element BioSampleAccn > data/{self.name}_temp_ids.txt")
         self.ids = []
         #lendo o arquivo temporário e extraindo os IDs
-        with open("temp_ids","r") as temp:
+        with open(f"data/{self.name}_temp_ids.txt","r") as temp:
             lines = temp.readlines()
             for line in lines:
                 line = line.rstrip("\n")
@@ -41,48 +37,73 @@ class Species():
                 self.ids.append(id)
         self.assembly_ids= [i[0] for i in self.ids]
         self.biosample_ids= [i[1] for i in self.ids]
-        os.system(f'rm {self.name}_temp_ids.txt')
+        os.remove(f'data/{self.name}_temp_ids.txt')
         print(f'\n\nIDs de {self.name} salvos na memória\n\n')
 
     def write_ids_files(self):
-        path = f'ids/{self.name}'
-        with open(path+f'/{self.name}_ids.txt','w') as ids:
+        self.path = f'data/ids/{self.name}'
+        if not os.path.exists(self.path):
+            os.mkdir(self.path)
+        with open(self.path+f'/{self.name}_ids.txt','w') as ids:
             ids.write(f'{self.name} | número de entradas: {self.count} | date: {datetime.now()}\n')
-            for i,j in self.assembly_ids,self.biosample_ids:    
-                ids.write(f'{i},{j}\n')
-        with open(path+f'/{self.name}_assembly_ids.txt', 'w') as assembly:
+            for i in self.ids:    
+                ids.write(f'{i[0]} {i[1]}\n')
+        with open(self.path+f'/{self.name}_assembly_ids.txt', 'w') as assembly:
             assembly.write(f'{self.name} | número de entradas: {self.count} | date: {datetime.now()}\n')
             for i in self.assembly_ids:
                 assembly.write(f'{i}\n')
-        with open(path+f'/{self.name}_biosample_ids.txt','w') as biosample:
+        with open(self.path+f'/{self.name}_biosample_ids.txt','w') as biosample:
             biosample.write(f'{self.name} | número de entradas: {self.count} | date: {datetime.now()}\n')
             for i in self.biosample_ids:
                 biosample.write(f'{i}\n')
-        print(f'\n\nIDs de {self.name} salvos em {path}\n\n')        
-    #nao esquecer de limpar os arquivos temporários
+        print(f'\n\nIDs de {self.name} salvos em {self.path}\n\n')        
+    
     def check_new_entries(self):
         # salvando o output do esearch
         esearch_out = subprocess.check_output(self.esearch,text=True)
         new_count =  esearch_out.split('</Count>')[0].split('<Count>')[1]
         if new_count != self.count:
             print(f'\n\n\nNOVAS {int(new_count)-int(self.count)} ENTRADAS DETECTADAS PARA' +f'{self.name}\n\n\n'.upper())
-        return True
+            return True
+        print("Nenhuma nova entrada detectada")
+        return False
     def update_ids(self):
-        with open(f'logs/{self.name}_log_ids.txt','r') as log:
+        with open(f'pathogens-ids/logs/{self.name}_log_ids.txt','r') as log:
             label = log.readline()
             last_date = label.split('| date: ')[1]
             last_date = last_date.split()[0].replace('-','/')
+            print("Última vez atualizado: "+last_date)
             new_esearch = ["esearch", "-db", "assembly" ,"-query" , f"((\"{last_date}\"[AsmUpdateDate] : \"3000\"[AsmUpdateDate]) AND \"{self.name}\"[Organism]) AND (latest[filter] AND \"complete genome\"[filter] AND all[filter] NOT anomalous[filter])"]
-            out_new_esearch =  subprocess.check_output(new_esearch,text=True).split('</Count>')[0].split('<Count>')[1]
+            new_esearch_str = " ".join(new_esearch)
+            out_new_esearch =  subprocess.check_output(new_esearch,text=True)
+            print(f'out: {out_new_esearch}')
             new_count = out_new_esearch.split('</Count>')[0].split('<Count>')[1]
-
+            #Arrumar aqui
             print(f'Novas {new_count} entradas detectadas')
-            os.system(f"echo {out_new_esearch}  | efetch -format docsum |xtract -pattern DocumentSummary -element AssemblyAccession -element BioSampleAccn > {self.name}_temp_ids.txt")
-            #ler arquivo e adicionar aos atributos corretos
+            os.system(f"{new_esearch_str} | efetch -format docsum |xtract -pattern DocumentSummary -element AssemblyAccession -element BioSampleAccn > data/{self.name}_temp_ids.txt")
+            with open(f'data/{self.name}_temp_ids.txt','r') as temp:
+                new_ids = temp.readlines()
+                new_ids = [i.split() for i in new_ids]
+                with open(f'{self.path}/{self.name}_ids.txt','a') as ids:
+                    for i in new_ids:
+                        ids.write(f'{i[0]} {i[1]}\n')
+                with open(f'{self.path}/{self.name}_assembly_ids.txt', 'a') as assembly:
+                    for i in new_ids:
+                        assembly.write(f'{i[0]}\n')
+                with open(self.path+f'/{self.name}_biosample_ids.txt','a') as biosample:
+                    for i in self.biosample_ids:
+                        biosample.write(f'{i[1]}\n')
 
-            
 
-            
+ed = Species('Edwardsiella_tarda')
+ed.extract_ids()
+print(ed.ids)
+print(ed.assembly_ids)
+print(ed.biosample_ids)
+ed.write_ids_files()
+ed.check_new_entries()         
+ed.log_ids()
+ed.update_ids()
 
 
 
