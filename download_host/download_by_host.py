3bb662c99ea3c292dac6caf2b0b11d68c740d5b3
Artur Pedro <arturpedromartins@gmail.com>
2023-05-11 16:46:48 -0300

    função get_biosamples_info() finalizada

--- download_host/download_by_host.py
+++ download_host/download_by_host.py
@@ -1,20 +1,28 @@
 import os
 import sys
+import json
+'''
 id_path = os.path.abspath(os.path.join(os.path.dirname(__file__), '../extract_ids'))
 sys.path.append(id_path)
 import ids_mining
-
+'''
 
 def get_biosamples_info():
+    # retorna um dicionário com o nome das espécies como chave e a lista de uids de biosamples como valor
     with open('extract_names/species_names.txt','r') as names:
         all_names = [i.rstrip('\n') for i in names.readlines()]
+        print(all_names)
+    biosamples_ids = {}
     for name in all_names:
-        with open(f'data/ids/{name}/{name}_biosample_ids.txt','r') as biosamples_ids:
-            ids = biosamples_ids.readlines()
-            ids = ids[1:]
-            for id in ids:
-                biosample = id.rstrip('\n')
-                info = os.system(f'efetch -db biosample -id {biosample} -format docsum | xtract -pattern DocumentSummary -element Attribute > download_host/{biosample}_info_temp.txt')
+        with open(f'data/ids/{name}/{name}_ids.json','r') as json_ids:
+            ids = json.load(json_ids)
+            ids = ids["entries"]
+            data = [ids[id] for id in ids]
+            biosamples_ids[name] = data
+    print(biosamples_ids)
+
+
+'''                info = os.system(f'efetch -db biosample -id {biosample} -format docsum | xtract -pattern DocumentSummary -element Attribute > download_host/{biosample}_info_temp.txt')
                 title = os.system(f'efetch -db biosample -id {biosample} -format docsum | xtract -pattern DocumentSummary -element Attribute@attribute_name > download_host/{biosample}_title_temp.txt')
                 with open(f'download_host/{biosample}_title_temp.txt','r') as titles:
                     with open(f'download_host/{biosample}_info_temp.txt','r') as infos:
@@ -26,7 +34,7 @@ def get_biosamples_info():
                         os.remove(f'download_host/{biosample}_title_temp.txt')
                         os.remove(f'download_host/{biosample}_info_temp.txt')
                 print(biosample_info)
-
+'''
                     
 
 
