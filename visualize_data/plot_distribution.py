daf3763288f9c85325d2f8aad1b21316115815d2
Artur Pedro <arturpedromartins@gmail.com>
2023-06-25 23:49:18 -0300

    Parâmetros melhorados e funções novas

--- visualize_data/plot_distribution.py
+++ visualize_data/plot_distribution.py
@@ -31,44 +31,65 @@ def write_pdf(fname, figures, resolution=(1366, 768),path='.'):
         fig.savefig(doc, format='pdf', dpi=dpi)
     doc.close()
 
-def plotSingleDistribution(name,key='species'):
+def plotSingleDistribution(name='',key='species',all=False):
+    
     metadata = f'data/groups_info/{name}/{name}_metadata'
-    print(name)
+    if all:
+        metadata = f'data/metadata.json'
+    # Verificando se existem dados válidos
     if os.path.exists(metadata):
     
+        # Acessando as informações
         with open(metadata,'r') as file:    
             data = js.load(file)
             species = data[key]
+            if len(species) == 0:
+                return False
+            # Filtrando valores muito baixos
+            values = list(species.values())
+            sum_data = sum(values)
+            filtered_species = {k:v for k,v in species.items() if v > sum_data/100}
 
             # Armazenando a informação em ordem crescente
-            sorted_species = sorted(species.items(), key=lambda x: x[1])
+            sorted_species = sorted(filtered_species.items(), key=lambda x: x[1])
+
+            # Separando os nomes e as contagens
             names = [item[0].replace(' ','\n') for item in sorted_species]
             count = [item[1] for item in sorted_species]
-            for idx,x in enumerate(count):
-                if sum(count)/100 > x:
-                    del names[idx]
-                    del count[idx]
-                    
+            
 
             # Normalizar os valores de contagem entre 0 e 1
             normalized_counts = np.array(count) / max(count)
 
-            # Criação do gráfico de barras verticais com cores proporcionais aos valores
+            # Criação da paleta de cores proporcionais aos valores
             colors = cm.Blues(normalized_counts)
             
+            # Iniciando a imagem
             fig = plt.figure()
+
+            # Iniciando gráfico
             plt.barh(names,count, color=colors, edgecolor='black')
+            
+            # Mostrando tamanho do lado das barras
             for i in range(len(count)):
-                plt.text(count[i], i, str(count[i]), ha='left', va='center')
+                plt.text(count[i], i, " "+str(count[i]), ha='left', va='center')
+
+            # Gerando grade
             plt.grid(True, axis='x', linestyle='--', alpha=0.9)
 
             plt.gca().set_axisbelow(True)
             
+            # Limites do gráfico
             plt.xlim(0, max(count))
             
+            # Pontos para serem plotados em X
             x_points = [i for i in set(count)]
             
-            if max(count) <= 50:
+            # Selecionando pontos para adicionar baseado na escala
+            if max(count) <= 25:
+                for i in range(0,max(count),5):
+                    x_points.append(i)
+            if max(count) <= 50 and max(count)>25:
                 for i in range(0,max(count),10):
                     x_points.append(i)
             if max(count) > 50 and max(count) < 100:
@@ -81,21 +102,37 @@ def plotSingleDistribution(name,key='species'):
                 for i in range(0,max(count),100):
                     x_points.append(i)
             
+            # Elimina pontos muito próximos no eixo x
             x_points = sorted(x_points)
-
+            print(x_points)
+            last_x = max(x_points)
             thrs_points = []
+
             for idx,x in enumerate(x_points):
                 if idx == 0:
                     thrs_points.append(x)
                 else:
-                    if x_points[idx-1] + 5 >= x:
-                        continue
+                    if last_x > 500:
+                        if x_points[idx-1] + 20 >= x:
+                            continue
+                        else:
+                            thrs_points.append(x)
+                    
+                    elif last_x > 100:
+                        if x_points[idx-1] + 5 >= x:
+                            continue
+                        else:
+                            thrs_points.append(x)
                     else:
-                        thrs_points.append(x)
-        
+                        if x_points[idx-1] + 3 >=x:
+                            continue
+                        else:
+                            thrs_points.append(x)
+
             x_points = [i for i in sorted(set(thrs_points))]
-            
             plt.xticks(x_points,x_points)
+            
+            # Labels
             plt.xlabel('Count')
             plt.ylabel(key)
             plt.title(f'{key} distribution')
@@ -103,42 +140,28 @@ def plotSingleDistribution(name,key='species'):
     else:
         print('Sem metadata disponível')
         return None
-for group in readGroupsNames():
+
+def writeAll():
+    for group in readGroupsNames():
+        species = [plotSingleDistribution(group,'species')]
+        hosts = [plotSingleDistribution(group,'hosts')]
+
+        if species:
+            write_pdf(f"sdis_{group}.pdf",species,path=f'data/groups_info/{group}')
+        if hosts:    
+            write_pdf(f"hdis_{group}.pdf",hosts,path=f'data/groups_info/{group}')
+
+def writeSingle(group):
     species = [plotSingleDistribution(group,'species')]
     hosts = [plotSingleDistribution(group,'hosts')]
-    if species[0]:
-        write_pdf(f"sdis_{group}.pdf",[plotSingleDistribution(group,'species')],path=f'data/groups_info/{group}')
-    if hosts[0]:    
-        write_pdf(f"hdis_{group}.pdf",[plotSingleDistribution(group,'hosts')],path=f'data/groups_info/{group}')
-'''
-# host distribution
-with open('data/groups_info/Bacillus_cereus_group/Bacillus_cereus_group_metadata','r') as file:
-    a = js.load(file)
-    species = a['hosts']
-    sorted_data = sorted(species.items(), key=lambda x: x[1])
-    print(sorted_data)
-    names = [item[0] for item in sorted_data]
-    count = [item[1] for item in sorted_data]
-    # Normalizar os valores de contagem entre 0 e 1
-    normalized_counts = np.array(count) / max(count)
+    if species:
+        write_pdf(f"sdis_{group}.pdf",species,path=f'data/groups_info/{group}')
+    if hosts:    
+        write_pdf(f"hdis_{group}.pdf",hosts,path=f'data/groups_info/{group}')
 
-    # Criação do gráfico de barras verticais com cores proporcionais aos valores
-    colors = cm.Blues(normalized_counts)  # Utiliza o color map 'viridis' para definir as cores
-    
-    plt.barh(names,count, color=colors, edgecolor='black')
-    plt.grid(True, axis='x', linestyle='--', alpha=0.9)
-    plt.gca().set_axisbelow(True)
-    plt.xlim(0, max(count))
-    x_points = [i for i in set(count)]
-    step = int(max(count)/10)
-    print(step)
-    for i in range(0,max(count),step):
-        x_points.append(i)
-    plt.xticks(x_points,x_points)
-    plt.xlabel('Count')
-    plt.ylabel('Species')
-    plt.title('Species distribution')
-    # Displaying the plot
-    plt.show()
-'''
-
+def writeGeneral():
+    species = [plotSingleDistribution(all=True)]
+    host = [plotSingleDistribution(key='hosts',all=True)]
+    write_pdf(f"sdis_all.pdf",species,path="data/")
+    write_pdf(f"hdis_all.pdf",host,path="data/")
+writeGeneral()
