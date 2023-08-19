7d9c63f907caa09a75f2b9733a9b7942d0386d67
Artur Pedro <arturpedromartins@gmail.com>
2023-08-19 15:51:04 -0300

    revisão geral do código

--- visualize_data/plot_distribution.py
+++ visualize_data/plot_distribution.py
@@ -5,17 +5,26 @@ import matplotlib.pyplot as plt
 import matplotlib.cm as cm
 from matplotlib.backends.backend_pdf import PdfPages
 import numpy as np
+
 # species distribution
 
 def readGroupsNames():
+    '''
+    Lê o nome dos patógenos presentes no Pathogen no NCBI
+    '''
     with open(os.path.normpath("data/groups/groups_name_id.json"),"r") as data:
         groups = js.load(data)
         return groups
 
 def write_pdf(fname, figures, resolution=(1366, 768),path='.'):
+    '''
+    Salva a imagem do gráfico em pdf
+    '''
+    if not os.path.exists(path):
+        os.mkdir(path)
     file_path = os.path.normpath(path + '/' + fname)
     doc = PdfPages(file_path)
-    for fig in figures:
+    for fig in figures: 
         fig_width, fig_height = fig.get_size_inches()
         dpi = fig.get_dpi()
 
@@ -29,16 +38,16 @@ def write_pdf(fname, figures, resolution=(1366, 768),path='.'):
         fig.set_size_inches(new_fig_width, new_fig_height)
 
         fig.savefig(doc, format='pdf', dpi=dpi)
+        plt.close(fig)
     doc.close()
 
 def plotSingleDistribution(name='',key='species',all=False):
-    print(name)
+    
     metadata = f'data/groups_info/{name}/{name}_metadata'
     if all:
         metadata = f'data/metadata.json'
     # Verificando se existem dados válidos
     if os.path.exists(metadata):
-    
         # Acessando as informações
         with open(metadata,'r') as file:    
             data = js.load(file)
@@ -83,7 +92,7 @@ def plotSingleDistribution(name='',key='species',all=False):
             plt.xlim(0, max(count))
             
             # Pontos para serem plotados em X
-            x_points = []
+            x_points = [i for i in set(count)]
             
             # Selecionando pontos para adicionar baseado na escala
             if max(count) <= 25:
@@ -98,16 +107,12 @@ def plotSingleDistribution(name='',key='species',all=False):
             if max(count) >= 100 and max(count) <= 500:
                 for i in range(0,max(count),25):
                     x_points.append(i)
-            if max(count) >= 500 and max(count) <1000:
+            if max(count) >= 500:
                 for i in range(0,max(count),100):
                     x_points.append(i)
-            if max(count) >= 1000:
-                for i in range(0,max(count),500):
-                    x_points.append(i)
-            '''
+            
             # Elimina pontos muito próximos no eixo x
             x_points = sorted(x_points)
-            print(x_points)
             last_x = max(x_points)
             thrs_points = []
 
@@ -133,7 +138,6 @@ def plotSingleDistribution(name='',key='species',all=False):
                             thrs_points.append(x)
 
             x_points = [i for i in sorted(set(thrs_points))]
-            '''
             plt.xticks(x_points,x_points)
             
             # Labels
@@ -142,39 +146,36 @@ def plotSingleDistribution(name='',key='species',all=False):
             plt.title(f'{key} distribution')
         return fig        
     else:
-        print('Sem metadata disponível')
+        print(f'Sem metadata disponível para {name}')
         return None
 
 def writeAll():
     for group in readGroupsNames():
         species = [plotSingleDistribution(group,'species')]
         hosts = [plotSingleDistribution(group,'hosts')]
-
-        if species == None:
-            write_pdf(f"sdis_{group}.pdf",species,path=f'data/groups_info/{group}')
-        if hosts == None:    
-            write_pdf(f"hdis_{group}.pdf",hosts,path=f'data/groups_info/{group}')
+        print(species, group)
+        print(hosts, group)
+        if species[0] != None:
+            write_pdf(f"sdis_{group}.pdf",species,path=f'data/plot/{group}')
+        if hosts[0] != None:    
+            write_pdf(f"hdis_{group}.pdf",hosts,path=f'data/plot/{group}')
 
 def writeSingle(group):
     species = [plotSingleDistribution(group,'species')]
     hosts = [plotSingleDistribution(group,'hosts')]
-    print(hosts)
     if species:
-        write_pdf(f"sdis_{group}.pdf",species,path=f'data/groups_info/{group}')
+        write_pdf(f"sdis_{group}.pdf",species,path=f'data/plot/{group}')
     if hosts:    
-        write_pdf(f"hdis_{group}.pdf",hosts,path=f'data/groups_info/{group}')
+        write_pdf(f"hdis_{group}.pdf",hosts,path=f'data/plot/{group}')
 
 def writeGeneral():
     species = [plotSingleDistribution(all=True)]
     host = [plotSingleDistribution(key='hosts',all=True)]
-<<<<<<< HEAD
-    write_pdf(f"sdis_all.pdf",species,path="data/")
-    write_pdf(f"hdis_all.pdf",host,path="data/")
-=======
-    if species:
-        write_pdf(f"sdis_all.pdf",species,path="data/")
-    if host:    
-        write_pdf(f"hdis_all.pdf",host,path="data/")
->>>>>>> 0ca9a9a8e68596eefb722efb001e77388b08a2a5
-writeGeneral()
-writeAll()
+    write_pdf(f"sdis_all.pdf",species,path="data/plot")
+    write_pdf(f"hdis_all.pdf",host,path="data/plot")
+
+def update():
+    writeAll()
+
+if __name__ == "__main__":
+    update()
