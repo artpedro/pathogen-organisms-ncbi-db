90450e7010cd3b2169b466b92feb9f79af70a940
Artur Pedro <arturpedromartins@gmail.com>
2023-10-07 21:08:22 -0300

    verificação de download adicionada

--- pathogen_retrieve/download_batch.py
+++ pathogen_retrieve/download_batch.py
@@ -67,8 +67,11 @@ def download_batch(batch,path):
 
     if not os.path.exists(path):
         os.mkdir(path)
-    for entry in batch:
 
+    files_in_folder = os.listdir(path)
+    for entry in batch:
+        if entry+".fna.gz" in files_in_folder:
+            continue
         # preciso adicionar um if para não baixar assemblies já baixados, talvez uma nova função
         page = f'{entry[0:3]}/{entry[4:7]}/{entry[7:10]}/{entry[10:13]}'
         genome_url = ftp_url + page
@@ -102,3 +105,4 @@ def download_all():
     for name in names:
         download_group(name)
 
+download_group("Streptococcus_pneumoniae")
